087bbc427965c51bc0168c3d8f461d2c88a57660
Leandro <80827721+LeandroGarza@users.noreply.github.com>
2022-04-01 10:30:42 -0300

    empezando funcion de division

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("hola mundo")
+
+	/*div, err := division(5, 0)
+	if err != nil {
+		fmt.Println("Error:", err.Error())
+		return
+	}
+	fmt.Println("Division:", div)*/
+
+}
