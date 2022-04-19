bad9311a7bdd466a4a00371e75faf45ebfcefff0
Leandro <80827721+LeandroGarza@users.noreply.github.com>
2022-04-19 17:43:22 -0300

    ejercicios de clase

--- defer/defer.go
+++ defer/defer.go
@@ -12,30 +12,45 @@ func main() {
 	defer fmt.Println("Hello")
 	defer fmt.Println("Hello 2")
 
+	if err := CreateFile("example.txt"); err != nil {
+		panic(err)
+	}
+
+	if err := WriteFile("example.txt", []byte("This is an example file")); err != nil {
+		panic(err)
+	}
+
+	bytes, err := ReadFile("example.txt")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(fmt.Sprintf("Content of file: %s", string(bytes)))
+
 }
 
 func CreateFile(path string) error {
 	file, err := os.Create(path)
-	defer file.Close()
-
 	if err != nil {
 		return err
 	}
-
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
 	return nil
 }
 
 func WriteFile(path string, content []byte) error {
 
 	if err := os.WriteFile(path, content, os.ModeAppend); err != nil {
-
 		return err
 	}
 
 	return nil
 }
 
-func readFile(path string) ([]byte, error) {
+func ReadFile(path string) ([]byte, error) {
 
 	file, err := os.Open(path)
 	if err != nil {
